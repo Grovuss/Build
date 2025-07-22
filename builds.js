8704fbc12c362d27f17af76ad92e1deec35f7acc
Grovuss <grovuss@gmail.com>
2025-07-22 15:19:11 -0500

    Update builds.js

--- builds.js
+++ builds.js
@@ -10,7 +10,7 @@ Perfect for building, testing, or sandbox-style experiments.
 ## Features
 
 - **Large flat grass field** — endless space for building, testing, and messing around
--  **Accurate proportions** — matches the original map's scale and simplicity
+- **Accurate proportions** — matches the original map's scale and simplicity
 - **Hidden room** — just like the original, tucked beneath the spawn area
 - **Minimal terrain clutter** — perfect for experiments, Redstone testing, or PvP
         `,
@@ -18,6 +18,29 @@ Perfect for building, testing, or sandbox-style experiments.
             'Flatgrass1.png',
             'Flatgrass2.png',
             'Flatgrass3.png'
+        ],
+        downloadUrl: 'https://drive.google.com/uc?export=download&id=1gcEe5yPzCaXcOS_isCobOV5Yh4P6Bg5l',
+        tags: ['Recreation', 'Sandbox']
+    },
+ { 
+        id: 'mc_construct', 
+        name: 'MC_Construct', 
+        description: `
+**A simple, open Minecraft recreation of GM_Construct from GMOD.**
+
+Perfect for building, testing, or sandbox-style experiments.
+
+## Features
+
+- **Accurate layout** — scaled to closely match the Source engine’s iconic geometry
+- **Natural elements integrated** — trees, bushes, and water features blend with the urban environment
+- **Ample flat surfaces and structures** — perfect for building, roleplay, or redstone contraptions
+- **Hidden room** — just like the original, tucked beneath some stairs
+        `,
+        images: [
+            'Construct1.png',
+            'Construct2.png',
+            'Construct3.png'
         ],
         downloadUrl: 'https://drive.google.com/uc?export=download&id=1gcEe5yPzCaXcOS_isCobOV5Yh4P6Bg5l',
         tags: ['Recreation', 'Sandbox']
